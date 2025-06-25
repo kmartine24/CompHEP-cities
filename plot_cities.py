be68b5e9e0999766fe2a81983e12ec8ca5540a30
kylam0705 <specialk0705@yahoo.com>
2025-06-25 13:40:35 -0700

    Included Undergraduate Universities

--- plot_cities.py
+++ plot_cities.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
-cities = [["Chicago",41.881832, -87.623177],
-          ["Boston", 42.355083, -71.065880],
-          ["Madison", 43.073051, -89.401230],
-          ["Austin", 30.266666, -97.733330],
+cities = [["Boston", 42.355083, -71.065880]]
+
+schoolCities = [["Madison", 43.073051, -89.401230],
+          ["Urbana-Champaign", 40.1106, -88.2073],
+          ["College Station", 30.60139, -96.31445]]
+
+homeCities = [["Austin", 30.266666, -97.733330],
           ["Knoxville", 35.964668, -83.926453],
-	  ["Urbana-Champaign", 40.1106, -88.2073]]
+          ["Chicago",41.881832, -87.623177]]
+
 scale = 5
 
 map = Basemap(llcrnrlon=-125,llcrnrlat=25,urcrnrlon=-65,urcrnrlat=50,
@@ -21,6 +25,12 @@ map.drawcountries()
 map.drawlsmask(land_color=(0, 0, 0, 0), ocean_color='xkcd:lightblue', lakes=True)
 
 # Get the location of each city and plot it
+for (city, latitude, longitude) in homeCities:
+    x, y = map(longitude, latitude)
+    map.plot(x, y, marker='o',color='plum')
+for (city, latitude, longitude) in schoolCities:
+    x, y = map(longitude, latitude)
+    map.plot(x, y, marker='o',color='plum')
 for (city, latitude, longitude) in cities:
     x, y = map(longitude, latitude)
     map.plot(x, y, marker='o',color='plum')
