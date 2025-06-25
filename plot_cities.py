bd163e2ff23c5710c0d7ff823ccbe6373a074799
kylam0705 <specialk0705@yahoo.com>
2025-06-25 11:28:25 -0700

    added US borders

--- plot_cities.py
+++ plot_cities.py
@@ -5,10 +5,13 @@ cities = [["Chicago",48, -100],
           ["Boston", 49, -90]]
 scale = 5
 
-map = Basemap(llcrnrlon=-119,llcrnrlat=22,urcrnrlon=-64,urcrnrlat=49,
-        projection='lcc',lat_1=32,lat_2=45,lon_0=-95)
+map = Basemap(llcrnrlon=-125,llcrnrlat=25,urcrnrlon=-65,urcrnrlat=50,
+#        projection='lcc',lat_1=32,lat_2=45,lon_0=-95)
+	projection = 'lcc', resolution = 'l', lat_0=38, lon_0=-98)
 
 map.drawstates()
+map.drawcoastlines()
+map.drawcountries()
 
 # Get the location of each city and plot it
 for (city, latitude, longitude) in cities:
